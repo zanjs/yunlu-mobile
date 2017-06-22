c2cd2343c467263f9d8bac65382bd656eaa30188
lyb <124553153@qq.com>
2017-06-22 09:10:48 +0800

    login and reg

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -36,6 +36,18 @@
                @click="goRoute('/enterprisecarte')">
                企业名片
     </mt-button>
+    <mt-button type="primary"
+               size="large"
+               class="btn-block"
+               @click="goRoute('/login')">
+      登录注册
+    </mt-button>
+    <mt-button type="primary"
+               size="large"
+               class="btn-block"
+               @click="goRoute('/regist')">
+      注册
+    </mt-button>
   </section>
 </template>
 
