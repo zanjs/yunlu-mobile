ddadbfe14b29cb67dd6a6d4ae4d921a2e501477c
jibenziliao <1501718947@qq.com>
2017-06-26 12:16:34 +0800

    Add Download.vue

--- src/views/download/Download.vue
+++ src/views/download/Download.vue
@@ -1,14 +1,26 @@
 <template>
   <section>
-    <mt-header title="下载"
-               fixed
-               class="header">
-      <mt-button slot="left"
-                 @click="goBack()"
-                 class="button-text">
-        <i class="iconfont icon-fanhui"></i>
-      </mt-button>
-    </mt-header>
+    <div class="top">
+      <div class="header">
+        <a @click="goBack()">
+          <i class="iconfont icon-fanhui"></i>
+        </a>
+        <a @click="goHome()">
+          <i class="iconfont icon-zhuye"></i>
+        </a>
+      </div>
+      <div class="download-bg">
+        <img src="../../assets/download.png">
+      </div>
+    </div>
+    <div class="download-btn">
+      <!--https://itunes.apple.com/cn/app/yun-lu/id1040721645?mt=8-->
+      <a href="http://www.yunlu6.com/yunlu.apk">下载云庐客户端</a>
+    </div>
+    <div class="tips">
+      <p>如果无法下载，你可以复制下面链接，用其他的浏览器打开</p>
+      <a href="http://www.yunlu6.com/yunlu.apk">http://www.yunlu6.com/yunlu.apk</a>
+    </div>
   </section>
 </template>
 
@@ -22,6 +34,9 @@
     methods: {
       goBack () {
         this.$router.go(-1)
+      },
+      goHome () {
+        this.$router.push({name: 'Home'})
       }
     },
     mounted () {
@@ -38,20 +53,65 @@
 <style lang="scss" scoped>
   @import '../../styles/mixin';
 
-  .header {
-    background-color: $green;
-    @include px2rem(height, 88px);
-    @include pm2rem(padding, 0px, 30px, 0px, 30px);
-    @include font-dpr(17px);
-    z-index: 1002 !important;
-    h1 {
-      @include font-dpr(17px);
+  .top {
+    position: relative;
+    .header {
+      z-index: 1001;
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      display: flex;
+      justify-content: space-between;
+      @include pm2rem(padding, 20px, 30px, 0px, 30px);
+      color: $white;
+      a {
+        @include px2rem(height, 68px);
+        @include px2rem(width, 68px);
+        @include px2rem(border-radius, 34px);
+        background-color: #49B495;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        line-height: 1;
+      }
+      a:active {
+        background-color: rgba(73, 180, 149, .4);
+      }
     }
-    .button-text {
-      @include font-dpr(15px);
+    .download-bg {
+      img {
+        @include px2rem(width, 750px);
+        @include px2rem(height, 539px);
+      }
     }
-    i {
-      @include font-dpr(20px);
+  }
+  .download-btn {
+    @include px2rem(margin-top, 92px);
+    text-align: center;
+    a {
+      display: block;
+      text-align: center;
+      margin: 0 auto;
+      @include px2rem(height, 86px);
+      @include px2rem(line-height, 86px);
+      @include px2rem(width, 500px);
+      @include font-dpr(16px);
+      background-color: #52CAA7;
+      color: $white;
+      @include px2rem(border-radius, 43px);
+    }
+  }
+  .tips {
+    text-align: center;
+    @include px2rem(margin-top, 255px);
+    p {
+      color: #A6A6A6;
+      @include font-dpr(12px);
+    }
+    a {
+      color: #3A7FD3;
+      @include font-dpr(12px);
     }
   }
 </style>
