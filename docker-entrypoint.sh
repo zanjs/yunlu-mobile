0ea2679cc79ea6a917bbd7d4a0ac41c8a5f318cc
Lyndon <snakeliwei@gmail.com>
2017-07-03 14:42:53 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,15 +6,15 @@ rm -rf /build/*
 npm run build
 cp -rf ./dist /build/pro
 #building test backend
-rm -rf ./dist/*
-sed -i "s/api.yunlu6.com/test.yunlu6.com/g" ./src/constants/constant.js
-npm run build
-cp -rf ./dist /build/test
+#rm -rf ./dist/*
+#sed -i "s/api.yunlu6.com/test.yunlu6.com/g" ./src/constants/constant.js
+#npm run build
+#cp -rf ./dist /build/test
 #building dev backend
-rm -rf ./dist/*
-sed -i "s/test.yunlu6.com/giant.dev.yunlu6.com/g" ./src/constants/constant.js
-npm run build
-cp -rf ./dist /build/dev
+#rm -rf ./dist/*
+#sed -i "s/test.yunlu6.com/giant.dev.yunlu6.com/g" ./src/constants/constant.js
+#npm run build
+#cp -rf ./dist /build/dev
 echo "======================================================"
 echo "=  Use mount local folder /build to get the file...  ="
 echo "======================================================"
