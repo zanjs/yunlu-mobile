1b7fc3d6e95b53098a56979669c22823b22c4f4a
jibenziliao <1501718947@qq.com>
2017-07-19 13:44:05 +0800

    Fix the bug for countDown in Login.vue

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,6 +1,6 @@
-export const URL_DEV = process.env.NODE_ENV === 'development' ? '' : 'https://test.yunlu6.com'
+export const URL_DEV = process.env.NODE_ENV === 'development' ? '' : 'https://api.yunlu6.com'
 export const REQUEST_TIME_OUT = 10000
 export const COUNT_DOWN_SECOND = 60
-export const VALID_CODE_IMG_URL = 'https://test.yunlu6.com/captcha'
-export const APP_ID = '3BXiD9Fga5RtswdyrJSFQ3h3-gzGzoHsz'
+export const VALID_CODE_IMG_URL = 'https://api.yunlu6.com/captcha'
+export const APP_ID = 'FALQWkhbIQhIj2AHpIWP6abh-gzGzoHsz'
 export const AUTHORIZATION_TIME = 120
