ba8daf11b36d5b252e4d5be3c82d7ac75ada1d84
jibenziliao <1501718947@qq.com>
2017-08-07 16:51:36 +0800

    Update mint-ui.css

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'mint-ui/lib/style.css'
+import './styles/iconfont.css'
 import App from './App'
 import router from './router'
 import MintUI from 'mint-ui'
-import 'mint-ui/lib/style.css'
-import './styles/iconfont.css'
 import store from './vuex/store'
 import { setStore } from './config/mUtils'
 import realtime from './config/leancloud'
