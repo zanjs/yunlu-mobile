533f7cd04ed0fee26359acc9ef2ade1da66d315e
jibenziliao <1501718947@qq.com>
2017-08-10 09:02:37 +0800

    Update css effect in InformationList.vue

--- src/components/common/InformationList.vue
+++ src/components/common/InformationList.vue
@@ -10,7 +10,7 @@
               error: 'http://oatl31bw3.bkt.clouddn.com/imgLoadingError.png',
               loading: 'http://oatl31bw3.bkt.clouddn.com/imgLoading3.jpg'
             }">
-        <div class="cover">
+        <div class="cover ellipsis">
           {{item.cnname}}（{{item.count}}）
         </div>
       </div>
@@ -79,11 +79,11 @@
         right: 0;
         background-color: rgba(0, 0, 0, .5);
         @include px2rem(height, 50px);
-        display: flex;
-        justify-content: center;
+        @include px2rem(width, 330px);
         color: $white;
         @include font-dpr(13px);
-        @include line-height(26px);
+        @include px2rem(line-height, 50px);
+        text-align: center;
       }
     }
   }
