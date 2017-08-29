5bea4af761dd4075a1a525825816fb3c95c27245
jibenziliao <1501718947@qq.com>
2017-08-29 10:44:05 +0800

    Add social auth module

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,6 +1,16 @@
-export const URL_DEV = process.env.NODE_ENV === 'development' ? '' : 'https://test.yunlu6.com'
+export const URL_DEV = process.env.NODE_ENV === 'development' ? '' : 'https://api.yunlu6.com'
 export const REQUEST_TIME_OUT = 10000
 export const COUNT_DOWN_SECOND = 60
-export const VALID_CODE_IMG_URL = 'https://test.yunlu6.com/captcha'
-export const APP_ID = '3BXiD9Fga5RtswdyrJSFQ3h3-gzGzoHsz'
+export const VALID_CODE_IMG_URL = 'https://api.yunlu6.com/captcha'
+export const APP_ID = 'FALQWkhbIQhIj2AHpIWP6abh-gzGzoHsz'
 export const AUTHORIZATION_TIME = 120
+export const QQ_AUTHORIZATION_CODE_URL = 'https://graph.qq.com/oauth2.0/authorize'
+export const QQ_LOGIN_APP_ID = 101419637
+export const QQ_LOGIN_APP_KEY = '23516448c9d10614f6f7648e5c315e5d'
+export const QQ_LOGIN_REDIRECT_URL = 'https://dw.yunlu6.com'
+export const WEIXIN_AUTHORIZATION_CODE_RUL = 'https://open.weixin.qq.com/connect/oauth2/authorize'
+export const WEIXIN_LOGIN_APP_ID = 'wx3f2ef5cdc3b9e961'
+export const WEIXIN_LOGIN_REDIRECT_URL = 'https://dw.yunlu6.com'
+export const WEIBO_LOGIN_APP_ID = '2694926337'
+export const WEIBO_AUTHORIZATION_CODE_URL = 'https://api.weibo.com/oauth2/authorize'
+export const WEIBO_LOGIN_REDIRECT_URL = 'https://dw.yunlu6.com'
