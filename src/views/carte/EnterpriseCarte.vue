7b31c6853607ac505c3222f7215ddd9fa372f2fd
jibenziliao <1501718947@qq.com>
2017-08-31 11:38:49 +0800

    Update goBack method in Login.vue

--- src/views/carte/EnterpriseCarte.vue
+++ src/views/carte/EnterpriseCarte.vue
@@ -403,7 +403,6 @@
       goEnterpriseDetail (id) {
         if (!this.hasLogin) {
           setStore('beforeLogin', 'true')
-          setStore('beforeLoginPath', this.$route.path)
           this.$router.push({name: 'Login'})
         } else {
           this.$router.push({name: 'EnterpriseDetail', params: {id: id}})
@@ -481,7 +480,6 @@
       }
     },
     mounted () {
-      console.log(this.$route)
       document.body.scrollTop = 0
       this.getEnterpriseDetail(this.teamId)
       this.handleSearchBar()
