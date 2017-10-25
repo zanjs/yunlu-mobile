02430fe3468db8730516e8f9e3275f0de005aa90
Lyndon <snakeliwei@gmail.com>
2017-10-25 11:47:49 +0800

    update url

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -4,4 +4,4 @@ export const COUNT_DOWN_SECOND = 60
 export const VALID_CODE_IMG_URL = 'https://dev.yunlu6.com:4443/captcha'
 export const LEANCLOUD_APP_ID = 'lrX8Sepx8KDaOW6TlmwasvV5-gzGzoHsz'
 export const AUTHORIZATION_TIME = 120
-export const AUTH_URL = 'https://dev.yunlu6.com'
+export const AUTH_URL = 'https://dev.yunlu6.com:4443'
