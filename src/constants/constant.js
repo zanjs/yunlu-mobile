f95519c73f502bb7a6c6c4d5029b3f66252cc680
Lyndon <snakeliwei@gmail.com>
2017-10-25 11:34:05 +0800

    update the dev url

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,7 +1,7 @@
-export const API_URL = process.env.NODE_ENV === 'development' ? '' : 'https://dev.yunlu6.com'
+export const API_URL = process.env.NODE_ENV === 'development' ? '' : 'https://dev.yunlu6.com：4443'
 export const REQUEST_TIME_OUT = 10000
 export const COUNT_DOWN_SECOND = 60
-export const VALID_CODE_IMG_URL = 'https://dev.yunlu6.com/captcha'
+export const VALID_CODE_IMG_URL = 'https://dev.yunlu6.com:4443/captcha'
 export const LEANCLOUD_APP_ID = 'lrX8Sepx8KDaOW6TlmwasvV5-gzGzoHsz'
 export const AUTHORIZATION_TIME = 120
 export const AUTH_URL = 'https://dev.yunlu6.com'
