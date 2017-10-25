becbe535f3dffebb0144ec65ee89e419253b24c7
Lyndon <snakeliwei@gmail.com>
2017-10-25 11:34:29 +0800

    update dev url

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NODE_ENV === 'development' ? '' : 'https://dev.yunlu6.com：4443'
+export const API_URL = process.env.NODE_ENV === 'development' ? '' : 'https://dev.yunlu6.com:4443'
 export const REQUEST_TIME_OUT = 10000
 export const COUNT_DOWN_SECOND = 60
 export const VALID_CODE_IMG_URL = 'https://dev.yunlu6.com:4443/captcha'
