1711120e08ed892cae01737a90c4ed9efab3b86d
Lyndon <snakeliwei@gmail.com>
2017-10-26 15:58:13 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,16 +4,7 @@
 rm -rf /build/test/
 #copy files to nginx 
 cp -rf /dist /build/test
-#building test backend
-#rm -rf ./dist/*
-#sed -i "s/api.yunlu6.com/test.yunlu6.com/g" ./src/constants/constant.js
-#npm run build
-#cp -rf ./dist /build/test
-#building dev backend
-#rm -rf ./dist/*
-#sed -i "s/test.yunlu6.com/giant.dev.yunlu6.com/g" ./src/constants/constant.js
-#npm run build
-#cp -rf ./dist /build/dev
+
 echo "======================================================"
 echo "=  Use mount local folder /build to get the file...  ="
 echo "======================================================"
